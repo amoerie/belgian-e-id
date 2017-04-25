8c9a715e1601e6e45b7280f3848c15a5755c18f2
tderycke <tom.derycke@live.be>
2017-04-25 22:23:03 +0200

    Add Government project

---
+++ workspace/Government/src/be/vub/government/Timestamp.java
@@ -0,0 +1,8 @@
+package be.vub.government;
+
+public class Timestamp {
+	public static void main ( String [] arguments )
+    {
+        System.out.println("Implement the signed Timestamp");
+    }
+}
