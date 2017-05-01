4f15602b88b1b6fec57f997d3f4b789bfed11c45
Alexander Moerman <alexander.moerman@gmail.com>
2017-05-01 16:44:37 +0200

    Update middleware project to try to connect to the Government service using the government certificate using a keystore which imported the government certificate

--- workspace/Middleware/src/be/vub/client/Client.java
+++ workspace/Middleware/src/be/vub/client/Client.java
@@ -5,10 +5,14 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
 
 import org.eclipse.swt.SWT;
@@ -24,18 +28,38 @@ public class Client {
 	public static void main(String[] args) {
 		try {
 			//socket connection to Government
-			System.setProperty("javax.net.ssl.trustStore", "eid.store");
-			Socket socket = ((SSLSocketFactory)SSLSocketFactory.getDefault()).createSocket("localhost",4444);
-			BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			//System.setProperty("javax.net.ssl.trustStore", "eid.store");
 			
-			PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-			String message = "message send to server";
-			printWriter.println(message);
+			System.setProperty("javax.net.debug", "ssl");
+			System.setProperty("javax.net.ssl.keyStoreType", "jks");
+			System.setProperty("javax.net.ssl.keyStore", "middleware.jks");
+			System.setProperty("javax.net.ssl.keyStorePassword", "ab123456");
+			System.setProperty("javax.net.ssl.trustStoreType", "jks");
+			System.setProperty("javax.net.ssl.trustStore", "middleware.jks");
+			System.setProperty("javax.net.ssl.trustStorePassword", "ab123456");
+			
+			SSLSocketFactory socketFactory = ((SSLSocketFactory)SSLSocketFactory.getDefault());
+			
+			URL url = new URL("https://localhost:4444");
+			HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+			connection.setSSLSocketFactory(socketFactory);
+
+			InputStream inputStream = connection.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			
+			//OutputStream outputStream = connection.getOutputStream();
+			PrintWriter printWriter = new PrintWriter(connection.getOutputStream(), true);
+			
+			System.out.print("Sending message to server: ");
+			printWriter.write("Hallo dit is de client");
+			
 			System.out.print("Message reply from server: ");
-			//System.out.println(socketBufferedReader.readLine());
+			String string = null;
+			while ((string = bufferedReader.readLine()) != null) {
+			    System.out.println("Received: " + string);
+			}
 			
-			//close socket after an event later on
-			socket.close();
 			
 			//open GUI
 			Client window = new Client();
