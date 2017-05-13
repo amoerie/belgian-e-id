16a9cb50ff0649bd4b5f8dbbf4ba0d2c44ee6b65
tderycke <tom.derycke@live.be>
2017-05-13 18:51:02 +0200

    Renamad the old service and make a new java project for ServiceProvider

---
+++ workspace/OLDServiceProvider/src/be/msec/service/Provider.java
@@ -0,0 +1,17 @@
+package be.msec.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Provider extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		//super.doGet(req, resp);
+		resp.getWriter().println("TESTING provider...");
+	}
+}
